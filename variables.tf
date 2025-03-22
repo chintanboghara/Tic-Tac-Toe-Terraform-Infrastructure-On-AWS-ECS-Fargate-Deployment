30e01e35438f4abed7d3c05a1f678c30a892190f
Chintan Boghara <chintansboghara@gmail.com>
2025-03-22 12:55:05 +0530

    Add Docker container variables to variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,19 @@
-# (If any global variables are required, define them here)
-variable "docker_registry_username" {
-  type      = string
-  description = "Optional username for Docker registry"
-  default   = ""
+variable "container_name" {
+  description = "Name of the Docker container"
+  type        = string
+  default     = "sample-app-container"
+}
+
+variable "image" {
+  description = "Docker image to deploy"
+  type        = string
+  default     = "nginx:latest"
+}
+
+variable "ports" {
+  description = "Map of container ports to host ports"
+  type        = map(number)
+  default     = {
+    "80" = 8080
+  }
 }
