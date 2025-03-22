7de1efa03ab40204424a08efdca7efa2d0d39929
Chintan Boghara <chintansboghara@gmail.com>
2025-03-22 15:41:13 +0530

    Remove output variables for Docker container from outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "container_id" {
-  description = "ID of the deployed Docker container"
-  value       = module.app_container.container_id
-}
-
-output "container_status" {
-  description = "Status of the deployed Docker container"
-  value       = module.app_container.container_status
-}
