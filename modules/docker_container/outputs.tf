69efd833a232d058f9e4b5ec79c756e522c06bad
Chintan Boghara <chintansboghara@gmail.com>
2025-03-22 15:42:27 +0530

    Remove outputs for Docker container module from Terraform configuration

--- modules/docker_container/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "container_id" {
-  description = "ID of the deployed Docker container"
-  value       = docker_container.app.id
-}
-
-output "container_status" {
-  description = "Status of the deployed Docker container"
-  value       = docker_container.app.status
-}
