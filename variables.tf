dce615273b22723f0038ed7e28369b73ef423c91
Chintan Boghara <chintansboghara@gmail.com>
2025-03-22 12:49:25 +0530

    Add variable for optional Docker registry username in Terraform configuration

---
+++ variables.tf
@@ -0,0 +1,6 @@
+# (If any global variables are required, define them here)
+variable "docker_registry_username" {
+  type      = string
+  description = "Optional username for Docker registry"
+  default   = ""
+}
