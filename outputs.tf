5f35d7759ba637cee4dac83bdde15efbcaa45cdc
Chintan Boghara <chintansboghara@gmail.com>
2025-03-22 12:55:02 +0530

    Refactor output variables for Docker container in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "nginx_container_id" {
-  description = "The ID of the deployed Nginx container"
-  value       = module.nginx_service.container_id
+output "container_id" {
+  description = "ID of the deployed Docker container"
+  value       = module.app_container.container_id
+}
+
+output "container_status" {
+  description = "Status of the deployed Docker container"
+  value       = module.app_container.container_status
 }
